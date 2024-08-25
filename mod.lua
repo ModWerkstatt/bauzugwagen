4ade08e2f8892bac70eac2723c703d88f8d9eee6
Florian Thiel <flth@flth.de>
2024-08-25 22:22:53 +0200

    initial

---
+++ mod.lua
@@ -0,0 +1,20 @@
+function data()
+return {
+	info = {
+		minorVersion = 1,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+			{
+				name = "ModWerkstatt",
+				role = "CREATOR",
+			},
+		},
+		tags = { "germany", "bauzug", "bahndienstwagen", "bundesbahn", "bahn", "wiebe" },
+	},
+	options = {
+	},
+}
+end
