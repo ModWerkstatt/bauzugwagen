a97727e3c58312550fc960a53cd32ee25767fb9d
Florian Thiel <flth@flth.de>
2025-03-11 14:35:14 +0100

    Strings ergänzt

--- strings.lua
+++ strings.lua
@@ -1,19 +1,20 @@
 function data()
 return {
 	en = {
+
 		["mod_name"] = "Construction train cars",
 		["mod_desc"] = "Various wagons for use in construction trains for transport and ballast as well as for official use, for overnight stays or for longer stays of people e.g. on construction sites.",
 
-        ["Bauzugwagen_menu"] = "Construction train (Facs | Fcs | Kbs | conversion cars)", 
-        ["leer"] = " (empty)",
-        ["halb"] = " (half loaded)",
-        ["voll"] = " (full loaded)",
+        ["Bauzugwagen_menu"] = "Construction train (Facs | Fcs | Kbs | conversion cars)",
+        ["leer"] = " [empty]",
+        ["halb"] = " [half loaded]",
+        ["voll"] = " [full loaded]",
         ["Dienstwagen3yg_name"] = "Company car conversion car",
         ["Dienstwagen3yg_desc"] = "Retired 3-axle conversion wagons were continued to be used as railway service vehicles.",
         ["Werkstattwagen3yg_name"] = "Workshop car conversion car",
         ["Werkstattwagen3yg_desc"] = "Retired 3-axle conversion wagons were continued to be used as railway service vehicles.",
         ["Facs124_bauzug_name"] = "Facs 124 Construction train DB AG",
-        ["Facs124_bauzug_desc"] = "Type Facs 124 self-unloading wagon with gravity discharge in yellow as a Deutsche Bahn AG construction train. Unloaded, half full or fully loaded with track ballast.",        
+        ["Facs124_bauzug_desc"] = "Type Facs 124 self-unloading wagon with gravity discharge in yellow as a Deutsche Bahn AG construction train. Unloaded, half full or fully loaded with track ballast.",
         ["Fcs090_wiebe_name"] = "Fcs 090 Wiebe",
         ["Fcs090_wiebe_desc"] = "Type Facs 124 self-unloading wagon with gravity discharge in yellow as of Deutsche Bahn AG construction train. Unloaded, half full or fully loaded with track ballast.",
         ["Fcs090_OeBB_name"] = "Fcs 090 ÖBB Infrastruktur",
@@ -26,12 +27,16 @@ return {
         ["Kls442_bauzug_name"] = "Kls 442 Rungenwagen DB",
         ["Kls442_bauzug_dbag_name"] = "Kls 442 Rungenwagen DB AG",
         ["Kls442_bauzug_desc"] = "Kls 442 type stake wagon without stakes. Unloaded or loaded with track ballast.",
+        ["Schotterwagen_Bauzug_name"] = "Schotterwagen",
+        ["Schotterwagen_desc"] = "Ballast wagons have two centrally arranged chutes in addition to the side chutes, so that the area between the tracks can also be ballasted. In addition to the actual function of transporting ballast, the purpose of the vehicle is also the targeted placement of the load.",
+
 	},
 	de = {
+
 		["mod_name"] = "Bauzugwagen",
 		["mod_desc"] = "Verschiedene Wagen für den Einsatz in Bauzügen zum Transport und Schotter sowie zum Dienstgebrauch, zur Übernachtung oder zum längeren Aufenthalt von Personen z.B. auf Baustellen.",
 
-        ["Bauzugwagen_menu"] = "Bauzug (Facs | Fcs | Kbs | Dienstwagen)", 
+        ["Bauzugwagen_menu"] = "Bauzug (Facs | Fcs | Kbs | Dienstwagen)",
         ["leer"] = " [unbeladen]",
         ["halb"] = " [halb beladen]",
         ["voll"] = " [voll beladen]",
@@ -40,7 +45,7 @@ return {
         ["Werkstattwagen3yg_name"] = "Werkstattwagen Umbauwagen",
         ["Werkstattwagen3yg_desc"] = "Ausgemusterte 3-achsige Umbauwagen wurden als Bahndienstwagen weitergenutzt.",
         ["Facs124_bauzug_name"] = "Facs 124 Bauzug DB AG",
-        ["Facs124_bauzug_desc"] = "Selbstentladewagen mit Schwerkraftentleerung der Bauart Facs 124 in gelb als Bauzug der Deutschen Bahn AG. Unbeladen, halb voll beladen oder voll beladen mit Gleisschotter.",        
+        ["Facs124_bauzug_desc"] = "Selbstentladewagen mit Schwerkraftentleerung der Bauart Facs 124 in gelb als Bauzug der Deutschen Bahn AG. Unbeladen, halb voll beladen oder voll beladen mit Gleisschotter.",
         ["Fcs090_wiebe_name"] = "Fcs 090 Wiebe",
         ["Fcs090_wiebe_desc"] = "Selbstentladewagen mit Schwerkraftentleerung der Bauart Fcs 090 in braun als Bauzug der Firma H.F. Wiebe. Unbeladen, halb voll beladen oder voll beladen mit Gleisschotter.",
         ["Fcs090_OeBB_name"] = "Fcs 090 ÖBB Infrastruktur",
@@ -53,6 +58,9 @@ return {
         ["Kls442_bauzug_name"] = "Kls 442 Rungenwagen DB",
         ["Kls442_bauzug_dbag_name"] = "Kls 442 Rungenwagen DB AG",
         ["Kls442_bauzug_desc"] = "Rungenwagen ohne Rungen der Bauart Kls 442. Unbeladen oder beladen mit Gleisschotter.",
+        ["Schotterwagen_Bauzug_name"] = "Schotterwagen",
+        ["Schotterwagen_desc"] = "Schotterwagen verfügen neben den seitlichen Schüttvorrichtungen über zwei mittig angeordnete Schütten, um auch den Bereich zwischen den Gleisen beschottern zu können. Neben der eigentlichen Funktion des Schottertransports ist als auch die gezielte Einbringung des Ladegutes Zweck des Fahrzeuges.",
+
 	}
 }
 end
